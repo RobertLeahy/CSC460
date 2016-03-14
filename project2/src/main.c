74c9b6fa51c3a4fb531ee040be31cc49735a2b5e
Robert Allan Hennigan Leahy <rleahy@rleahy.ca>
2016-03-14 10:53:01 -0700

    Project 2 - Test 2
    When compiling with the preprocessor macro TEST equal to 2 the second provided test is now built.

--- project2/src/main.c
+++ project2/src/main.c
@@ -72,6 +72,72 @@ void a_main(void)
 	Task_Create(Task_P3, 3, 0);
 }
 
+#elif TEST==2
+
+//
+// LAB - TEST 2
+//	Noah Spriggs, Murray Dunne, Daniel McIlvaney
+
+// EXPECTED RUNNING ORDER: P1,P2,P3,P1,P2,P3,P1
+//
+// P1 sleep              lock(attept)            lock
+// P2      sleep                     resume
+// P3           lock suspend               unlock
+
+MUTEX mut;
+EVENT evt;
+volatile PID pid;
+
+void Task_P1(void)
+{
+	Task_Sleep(10); // sleep 100ms
+	Mutex_Lock(mut);
+    for(;;);
+}
+
+void Task_P2(void)
+{
+	Task_Sleep(20); // sleep 200ms
+	Task_Resume(pid);
+    for(;;);
+}
+
+void Task_P3(void)
+{
+	Mutex_Lock(mut);
+	Task_Suspend(pid);
+	Mutex_Unlock(mut);
+    for(;;);
+}
+
+void a_main(void)
+{
+	/*
+	//Place these as necessary to display output if not already doing so inside the RTOS
+	//initialize pins
+	DDRB |= (1<<PB1);	//pin 52
+	DDRB |= (1<<PB2);	//pin 51	
+	DDRB |= (1<<PB3);	//pin 50
+	
+	
+	PORTB |= (1<<PB1);	//pin 52 on
+	PORTB |= (1<<PB2);	//pin 51 on
+	PORTB |= (1<<PB3);	//pin 50 on
+
+
+	PORTB &= ~(1<<PB1);	//pin 52 off
+	PORTB &= ~(1<<PB2);	//pin 51 off
+	PORTB &= ~(1<<PB3);	//pin 50 off
+
+	*/
+	mut = Mutex_Init();
+	evt = Event_Init();
+
+	Task_Create(Task_P1, 1, 0);
+	Task_Create(Task_P2, 2, 0);
+	pid = Task_Create(Task_P3, 3, 0);
+}
+
 #else
 
 #error TEST specifies invalid test number
