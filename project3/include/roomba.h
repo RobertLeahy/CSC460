0676716a6cda9299eee4579183fddf6b80e511f4
Robert Allan Hennigan Leahy <rleahy@rleahy.ca>
2016-03-24 11:56:37 -0700

    Project 3 - roomba_create & roomba_destroy Documentation

--- project3/include/roomba.h
+++ project3/include/roomba.h
@@ -38,5 +38,30 @@ struct roomba_opt {
 };
 
 
+/**
+ *	Initializes a roomba structure and sets up all
+ *	appropriate hardware to communicate with a Roomba.
+ *
+ *	\param [out] r
+ *		A pointer to a roomba structure which shall be
+ *		initialized.
+ *	\param [in] opt
+ *		A roomba_opt structure which specifies the details
+ *		of the initialization.
+ *
+ *	\return
+ *		0 if the call succeeded, -1 otherwise.
+ */
 int roomba_create (struct roomba * r, struct roomba_opt opt);
+/**
+ *	Cleans up a roomba structure and releases all hardware
+ *	resources associated with it.
+ *
+ *	\param [in] r
+ *		A pointer to a roomba structure which shall be cleaned
+ *		up.
+ *
+ *	\return
+ *		0 if the call succeeded, -1 otherwise.
+ */
 int roomba_destroy (struct roomba * r);
