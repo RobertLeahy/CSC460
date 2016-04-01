226794ee6c585199bda3fefbc98da37714fcf613
Robert Allan Hennigan Leahy <rleahy@rleahy.ca>
2016-04-01 16:05:20 -0700

    Project 3 - Rudimentary Manual Roomba Control

--- project3/src/remote.c
+++ project3/src/remote.c
@@ -16,8 +16,41 @@ struct remote_state {
 	int8_t y;
 	bool button;
 	uart_t uart;
+	struct roomba roomba;
+	bool dirty;
 	
 };
+
+
+static void roomba (void * ptr) {
+	
+	struct remote_state * state=(struct remote_state *)ptr;
+	
+	for (;;) {
+		
+		if (mutex_lock(state->mutex)!=0) error();
+		
+		bool dirty=state->dirty;
+		state->dirty=false;
+		int16_t y=state->y;
+		
+		if (mutex_unlock(state->mutex)!=0) error();
+		
+		y*=4;
+		if (y>500) y=500;
+		if (y<-500) y=-500;
+		
+		if (dirty) {
+			
+			if (roomba_drive_direct(&state->roomba,y,y)!=0) error();
+			
+		}
+		
+		if (sleep(state->period)!=0) error();
+		
+	}
+	
+}
 
 
 static void uart (void * ptr) {
@@ -51,8 +84,11 @@ static void uart (void * ptr) {
 		
 		if (mutex_lock(state->mutex)!=0) error();
 		
+		if (state->x!=x) state->dirty=true;
 		state->x=x;
+		if (state->y!=y) state->dirty=true;
 		state->y=y;
+		if (state->button!=button) state->dirty=true;
 		state->button=button;
 		
 		if (mutex_unlock(state->mutex)!=0) error();
@@ -79,7 +115,7 @@ void a_main (void) {
 	
 	struct remote_state state;
 	memset(&state,0,sizeof(state));
-	state.uart=1;
+	state.uart=2;
 	state.period.tv_nsec=20000000ULL;
 	if (mutex_create(&state.mutex)!=0) error();
 	
@@ -88,8 +124,16 @@ void a_main (void) {
 	opt.baud=9600;
 	if (uart_init(state.uart,opt)!=0) error();
 	
+	struct roomba_opt ropt;
+	ropt.uart=1;
+	if (roomba_create(&state.roomba,ropt)!=0) error();
+	if (roomba_safe(&state.roomba)!=0) error();
+	
 	//	DEBUGGING
 	if (uart_init(0,opt)!=0) error();
+	
+	if (thread_set_priority(thread_self(),0)!=0) error();
+	if (thread_create(0,roomba,0,&state)!=0) error();
 	
 	uart(&state);
 	
