629613191bb02c386b013704fba365c04710707f
Robert Allan Hennigan Leahy <rleahy@rleahy.ca>
2016-04-01 18:46:28 -0700

    Project 3 - Complete Error Handling

--- project3/src/error.c
+++ project3/src/error.c
@@ -4,6 +4,41 @@
 #include <os.h>
 
 
+static void on (void) {
+	
+	PORTB|=1<<PB7;
+	
+}
+
+
+static void off (void) {
+	
+	PORTB&=~(1<<PB7);
+	
+}
+
+
+static void wait (unsigned int num) {
+	
+	for (unsigned int n=0;n<num;++n) for (unsigned int i=0;i<32000U;++i) asm volatile ("nop");
+	
+}
+
+
+static void blink_number (unsigned int num) {
+	
+	for (unsigned int n=0;n<num;++n) {
+		
+		on();
+		wait(4);
+		off();
+		wait(4);
+		
+	}
+	
+}
+
+
 void error (void) {
 	
 	//	Disabling interrupts freezes the kernel
@@ -19,9 +54,11 @@ void error (void) {
 	
 	for (;;) {
 		
-		//	TODO
-		(void)e;
-		(void)s;
+		wait(25);
+		
+		blink_number(e);
+		wait(10);
+		blink_number(s);
 		
 	}
 	
